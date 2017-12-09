c6a01bc17e2532ab99cd0c4989ba93ddaee9b9ef
sanjit <sanjitschouhan@gmail.com>
2017-12-09 22:58:20 +0530

    Created Factorial Program in Rust

---
+++ factorial.rs
@@ -0,0 +1,22 @@
+use std::io;
+
+fn main() {
+    println!("Enter a Number: ");
+    let mut num = String::new();
+
+    io::stdin()
+        .read_line(&mut num)
+        .expect("Error reading number");
+
+    let n = num.trim()
+                .parse::<u32>()
+                .unwrap();
+
+    let mut factorial = 1;
+    
+    for i in 1..n+1 {
+        factorial *= i;
+    }
+
+    println!("Factorial is {}",factorial);
+}
