522f091958980758d3d3d497249663d43c76851a
murarishettybhanu <34617423+murarishettybhanu@users.noreply.github.com>
2017-12-17 12:54:22 +0530

    Using this we can convert Fahrenheit value to Celsius

---
+++ fahrenheit_to_celsius.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn read_int()->f32{
+	let mut num=String::new();
+	
+	io::stdin()
+		.read_line(&mut num)
+		.expect("error");
+	
+	num.trim().parse::<f32>()
+		.unwrap()
+}
+fn main()
+{
+println!("enter temperature in fahrenheit");
+let mut fahrenheit = read_int();
+let mut celsius = ((a-32.0)*5.0)/9.0;
+println!("celsius is {}",b);
+}
