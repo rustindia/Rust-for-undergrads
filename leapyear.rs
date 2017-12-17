b5a27e6b0dc9ab00950fa09f8bec04713f4ceabd
abhisheksonnakula <32173393+abhisheksonnakula@users.noreply.github.com>
2017-12-18 00:47:16 +0530

    This program will tell you whether the given year is leap year or not

---
+++ leapyear.rs
@@ -0,0 +1,26 @@
+//This program will tell you whether the given year is leap year or not 
+use std::io;
+fn read_int()->u32{                   // function to read values
+	let mut num=String::new();
+	
+	io::stdin()
+		.read_line(&mut num)
+		.expect("error");
+	
+	num.trim().parse::<u32>()
+		.unwrap()
+}
+
+fn main(){
+let mut year=read_int();               // taking input year from user
+if year%4==0 && year%100!=0{          // condition for a leap year
+println!("{} is leap year",year);
+}
+else if year%100 == 0{               // condition for a leap year
+println!("{} is leap year",year);
+
+}
+else{
+println!("{} is not leap year",year);
+}
+}
