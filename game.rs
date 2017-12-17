f86781f859703051e14a1f8248ca3a69c003a325
sai bhavani kumar <sai.bhavanikumar6393@gmail.com>
2017-12-17 12:26:22 +0530

    Comparison of two Strings in rust

---
+++ game.rs
@@ -0,0 +1,41 @@
+use std::io;
+
+ fn read()->u32{
+      let mut num=String::new();
+      
+        io::stdin()
+                .read_line(&mut num)
+                .expect("error");
+             num.trim().parse::<u32>()
+                       .unwrap()
+ }
+ 
+ fn main()
+     {
+      println!("User 1 enter the Number");
+       let a=read();
+       
+       println!("User 2 enter the Number");
+        let b=read();
+        
+       println!("User 3 enter the Number");
+        let c=read();
+        
+if a==b
+     {
+       println!("User 2 gussed it Right");
+   }
+   
+   else {
+        
+        if a==c
+            {
+               println!("User 3 gussed it Right");
+          }
+  
+            else
+                 {
+                     println!(" Both gussed it Wrong");
+            } 
+      }
+}
