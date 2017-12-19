205d46fc423feae506b4e4dcd292de9c13267e8b
abhisheksonnakula <32173393+abhisheksonnakula@users.noreply.github.com>
2017-12-20 00:01:54 +0530

    Program to Calculate Average Using vector

---
+++ average.rs
@@ -0,0 +1,33 @@
+/* Program to Calculate Average Using vector */
+
+use std::io;
+fn read_int() -> i32 {
+    let mut num = String::new();
+
+    io::stdin()
+        .read_line(&mut num)
+        .expect("Error reading number");
+
+    num.trim()
+        .parse::<i32>()
+        .unwrap()
+}
+
+fn main(){
+let mut sum=0;
+println!("Enter the size of the array: ");
+let size = read_int();
+println!("Enter {} numbers of array: ",size);
+let mut v=vec!();
+ 
+ for _i in 0..size
+{
+ println!("Enter number {}: ",_i+1);
+ let x = read_int();
+ v.push(x);
+ sum=sum+x;
+}
+let average = sum/size;
+println!("The average of {:?}  numbers is {}",v,average);
+
+}
