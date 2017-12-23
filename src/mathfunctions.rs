f1bcf1e3fffcf3b1e5960908807be1acf3230f71
Asif khan <aisf.khan.399@gmail.com>
2017-12-23 16:44:43 +0530

    various math function example

---
+++ src/mathfunctions.rs
@@ -0,0 +1,12 @@
+use std::io::stdin;
+fn main(){
+
+    let mut neg=-4i32;
+    println!("{}",neg.abs());
+    println!("{}",neg.round());
+    println!("{}",neg.tan());
+    println!("{}",neg.cos());
+    
+    //how to use mathematical functions is written.
+    //there are lot of mathematical functions. just check docs
+}
