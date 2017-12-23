2d22ce9b3d8c673570dd740ecdd541fc166e3cc8
Asif khan <aisf.khan.399@gmail.com>
2017-12-23 16:39:48 +0530

    how to use white space, different forms of anumber

---
+++ src/differentacessingways.rs
@@ -0,0 +1,7 @@
+use std::io;
+fn main(){
+let (first,last)=("Asif","Khan");
+println!("{}{}",first,last);
+println!("B : {:b} H : {:x}",10,20); // Acessing elements is various forms
+println!("{ten:>ws$}",ten="Asif",ws=5); // whitespace function
+}
