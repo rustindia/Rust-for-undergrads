77f52a3db1168e88f2ef999f2aa8b15c2a8d436f
Asif khan <aisf.khan.399@gmail.com>
2017-12-23 16:50:41 +0530

    sum of numbers

---
+++ src/sumofnnumbers.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn read_int()->u32{
+	let mut num=String::new();
+	
+	io::stdin()
+		.read_line(&mut num)
+		.expect("error");
+	
+	num.trim().parse::<u32>()
+		.unwrap()
+}
+fn main(){
+let x=read_int();
+println!("{}",x*(x+1)/2);
+}
