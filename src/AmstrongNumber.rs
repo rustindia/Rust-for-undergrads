972f1a2c95f0572668bb0bd925e0faf77b16e8d3
* <Farzeen>
2017-12-28 06:22:38 +0530

    improved indentation.

--- src/AmstrongNumber.rs
+++ src/AmstrongNumber.rs
@@ -1,29 +1,31 @@
 use std::io;
 fn main(){
+
     let mut originalnumber;
     let mut remainder;
     let mut result = 0;
 
-println!("Enter a three digit integer ");
-let mut num=String::new();
-io::stdin()
-.read_line(&mut num)
-.expect("failed to read from stdin");
-let num=num.trim();
-let number: i32 = num.parse().unwrap();
+    println!("Enter a three digit integer ");
+    let mut num=String::new();
+    io::stdin()
+        .read_line(&mut num)
+        .expect("failed to read from stdin");
+    let num=num.trim();
+    let number: i32 = num.parse().unwrap();
 
-originalnumber = number;
+    originalnumber = number;
 
-while originalnumber != 0
-{
-    remainder = originalnumber%10;
-    result += remainder*remainder*remainder;
-    originalnumber /= 10;
-}
+    while originalnumber != 0 {
+        remainder = originalnumber%10;
+        result += remainder*remainder*remainder;
+        originalnumber /= 10;
+    }
 
-if result == number
-    {println!("{} is an Armstrong number.",number);}
-else
-    {println!("{} is not an Armstrong number.",number);}
+    if result == number {
+        println!("{} is an Armstrong number.",number);
+    }
+    else {
+        println!("{} is not an Armstrong number.",number);
+    }
 
 }
