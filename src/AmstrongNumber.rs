55e44acc6c751ea461ca5b77f5e234abbe801a49
Farzeen <farseenabdulsalam@gmail.com>
2017-12-28 06:58:02 +0530

    Improved variable names. Added explanation of program logic.

--- src/AmstrongNumber.rs
+++ src/AmstrongNumber.rs
@@ -1,31 +1,46 @@
 use std::io;
+
+/* An n digit number is an Amstrong number
+ * if the sum of nth powers of its digits
+ * equals that number
+ */
+
 fn main(){
 
-    let mut originalnumber;
-    let mut remainder;
-    let mut result = 0;
+    // TODO: Generalize this to n digits.
 
-    println!("Enter a three digit integer ");
-    let mut num=String::new();
+    println!("Enter a three digit integer: ");
+    let mut user_input = String::new();
     io::stdin()
-        .read_line(&mut num)
-        .expect("failed to read from stdin");
-    let num=num.trim();
-    let number: i32 = num.parse().unwrap();
+        .read_line(&mut user_input)
+        .expect("Failed to read from STDIN.");
+    let user_input = user_input.trim();
+    if user_input.len() != 3 {
+        panic!("The entered number is not three digits.");
+    }
+    let number: i32 = user_input.parse().unwrap();
 
-    originalnumber = number;
-
-    while originalnumber != 0 {
-        remainder = originalnumber%10;
-        result += remainder*remainder*remainder;
-        originalnumber /= 10;
+    let mut number_copy = number;
+    let mut result = 0;
+    let mut ones_digit;
+    
+    while number_copy != 0 {
+        // get one's digit of number using modulo arithmetic.
+        ones_digit = number_copy%10;
+        // add cube of that to a result variable.
+        result += ones_digit.pow(3);
+        // shift the number by one digit to the left,
+        // so that we can continue extracting one's digit.
+        // (discards any fractional part because this is integer division)
+        number_copy /= 10;
+        // when the last digit is shifted to left, number becomes 0
+        // and the loop exits
     }
 
     if result == number {
-        println!("{} is an Armstrong number.",number);
-    }
-    else {
-        println!("{} is not an Armstrong number.",number);
+        println!("{} is a three digit Armstrong number.",number);
+    } else {
+        println!("{} is not a three digit Armstrong number.",number);
     }
 
 }
