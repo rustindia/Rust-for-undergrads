9322b26fb6e7eb9dc45a810d0be7c6f569d09d12
Farzeen <farseenabdulsalam@gmail.com>
2017-12-28 18:12:56 +0530

    Improved indentation. Added comments.

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -1,55 +1,58 @@
-/* binary search is a search algorithm that finds the position of a target value within a sorted array. */
+
+/* Vinary search is a search algorithm that 
+ * finds the position of a target value within
+ * a sorted array.
+ */ 
 
 use std::io;
+
 fn read_int() -> i32 {
     let mut num = String::new();
 
     io::stdin()
         .read_line(&mut num)
-        .expect("Error reading number");
+        .expect("Error reading number.");
 
     num.trim()
         .parse::<i32>()
         .unwrap()
 }
-fn main()
-{
-	println!("enter number of elements");
-	let  n =read_int();
-	println!("enter {} integer",n);
-	let mut v=vec!();
-	for i in 0..n
-	{
-		println!("enter {} number",i+1);
-		let  x=read_int();
+
+fn main() {
+	println!("Enter number of elements:");
+	let  n = read_int();
+	println!("Enter {} numbers in increasing order: ",n);
+	let mut v = vec!();
+	for i in 0..n {
+		println!("Enter {}th number: ",i+1);
+		let x = read_int();
 		v.push(x);
 	}
-	println!("enter value to search");
-	let  search=read_int();
+	println!("Enter value to search: ");
+	let search = read_int();
 	let mut first:usize = 0;
 	let mut last:usize = (n as usize)-1;
-	let mut middle: usize=(first+last)/2;
+	let mut middle: usize = (first+last)/2;
 
 
-	while first<=last
-	{
-		if  v[middle]<search
-		{
+	while first<=last {
+		if v[middle]<search {
+            // if element at the middle is less than
+            // the element to be searched, search the
+            // right half of the array.
 			first = middle+1;
-		}
-		else if v[middle] == search
-		{
+		} else if v[middle] == search {
 			println!("{} found at location {}",search,middle+1);
 			break;
-		}
-		else
-		{
+		} else {
+            // if the eleemnt to be searched is less than
+            // the middle element, search the right half.
 			last = middle-1;
 		}
-		middle=  (first + last)/2;
+        // recalculate middle, because first or last has changed.
+		middle =  (first + last)/2;
 	}
-	if first > last
-	{
-		println!("not found!");
+	if first > last {
+		println!("Could not find {} in the entered list.", search);
 	}
 }
--- src/bubble_sort.rs
+++ src/bubble_sort.rs
@@ -1,39 +1,41 @@
 use std::io; 
-fn read()->i32{
-      let mut num=String::new();
-      
-        io::stdin()
-                .read_line(&mut num)
-                .expect("error");
-             num.trim().parse::<i32>()
-                       .unwrap()
+
+/* Read a list of numbers and 
+ * sort using bubble sort algorithm.
+ */
+
+fn read()->i32 {
+    let mut num=String::new();
+    io::stdin()
+        .read_line(&mut num)
+        .expect("Error");
+     num.trim()
+        .parse::<i32>()
+        .unwrap()
 }
-fn main(){
-         let size=read() as usize;
-         let mut a=vec!();
-         let mut temp:i32=0;
-         
-          for _k in 0..size{
-                   let x = read();
-                   a.push(x);
-           }
-         
-         for i in 0..size-1{
-               for j in 0..size-i-1{
-                     
-                     if(a[j]>a[j+1]){
-                        temp=a[j];
-                        a[j]=a[j+1];
-                        a[j+1]=temp;
-                                            
-                        
-                     }
-                      
-               }         
-          }
-          for l in 0..size{
-                println!("{}",a[l]);
-                
-          }
+
+fn main() {
+
+    println!("Enter the number of elements you wish to sort:");
+    let size = read() as usize;
+    let mut a = vec!();
+
+    println!("Enter the {} numbers:", size);
+    for _k in 0..size { 
+       let x = read();
+       a.push(x);
+    }
+
+    for i in 0..size-1 {
+       for j in 0..size-i-1 {
+             if a[j]>a[j+1] {
+                 a.swap(j,j+1);
+             }
+       }         
+    }
+    println!("Here are the sorted numbers:");
+    for l in 0..size {
+        println!("{}",a[l]);
+    }
 
 }
