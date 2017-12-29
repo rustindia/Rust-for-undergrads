dc2329d6c6abe1a6bc91779184d9040cc03ad506
Farzeen <farseenabdulsalam@gmail.com>
2017-12-29 20:16:36 +0530

    Added documentation.

--- src/calculate_bmi.rs
+++ src/calculate_bmi.rs
@@ -1,23 +1,29 @@
 use std::io;
 
- fn read()->f32{
-      let mut num=String::new();
-      
-        io::stdin()
-                .read_line(&mut num)
-                .expect("error");
-             num.trim().parse::<f32>()
-                       .unwrap()
+/* Calculates Body Mass Index using the formula:
+ *   bmi = weight in kg ÷(height in metre)^2
+ */
+
+ fn read() -> f32 {
+    let mut num=String::new();
+    io::stdin()
+        .read_line(&mut num)
+        .expect("error");
+    num.trim()
+        .parse::<f32>()
+        .unwrap()
  }
+
  fn main()
  {
-     println!("Enter Weight in Kilograms");
-    let a=read();
+    println!("Enter Weight in Kilograms: ");
+    let weight_kg = read();
     
-    println!("Enter Height in CentiMeters ");
-    let b=read();
+    println!("Enter Height in Centimetres: ");
+    let height_cm = read();
+    let height_m = height_cm/100.0;
      
-     let d=a/(b*b)*100.0*100.0;
+    let bmi = weight_kg/height_m.powi(2);
 
-     println!("Your BMI is {}",d);
+    println!("Your BMI is {}",bmi);
  }
