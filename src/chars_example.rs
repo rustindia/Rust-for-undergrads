3792a0d339d63f78c1f24ff226f16d57fe3d072f
Farzeen <farseenabdulsalam@gmail.com>
2017-12-29 20:19:08 +0530

    Fixed indentation.

--- src/chars_example.rs
+++ src/chars_example.rs
@@ -1,20 +1,22 @@
 use std::{i8,i16,u8};
 use std::io::stdin;
-fn main(){
-let mut random="Asif Khan";
-let mut count=random.chars().count();
-println!("{}",count);
-let mut charecters=random.chars();
-let mut ind_char=charecters.next();
 
-while(count<=random.chars().count()){
-    match ind_char{
-    Some(x)=>println!("{}",x),
-    None => break,
-}
-ind_char=charecters.next();
-count-=1;
-}
+fn main(){
+    let mut random="Asif Khan";
+    let mut count=random.chars().count();
+    println!("{}",count);
+
+    let mut characters=random.chars();
+    let mut ind_char=characters.next();
+
+    while(count<=random.chars().count()){
+        match ind_char {
+            Some(x) => println!("{}",x),
+            None => break,
+        }
+        ind_char=characters.next();
+        count-=1;
+    }
 }
 
 // PRORAM WRITTEN TO GIVE CHAR COUNT AND PRINT CHARS
