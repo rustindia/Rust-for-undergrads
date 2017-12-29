9ecd9b17c623c1d5181387cb154001b91812f9b8
Farzeen <farseenabdulsalam@gmail.com>
2017-12-29 20:41:18 +0530

    Fixed indentation. Added comments.

--- src/division.rs
+++ src/division.rs
@@ -1,5 +1,6 @@
-//this program is used to compute quotient and remainder for user input numbers
 use std::io;
+// This program is used to compute 
+// quotient and remainder for user input numbers
 fn read_int() -> i32 {
     let mut num = String::new();
 
@@ -11,14 +12,14 @@ fn read_int() -> i32 {
         .parse::<i32>()
         .unwrap()
 }
-fn main()
-{
-println!("enter dividend:");
-let dividend=read_int();              // reading an integer for dividend
-println!("divisor:");
-let divisor=read_int();               //reading an integer for divisor
-let quotient=dividend/divisor;        //calculating quotient
-let remainder=dividend%divisor;       //calculating remainder
-println!("Quotient={}",quotient);
-println!("Remainder={}",remainder);
+
+fn main() {
+    println!("enter dividend:");
+    let dividend = read_int();              // reading an integer for dividend
+    println!("divisor:");
+    let divisor = read_int();               //reading an integer for divisor
+    let quotient = dividend/divisor;        //calculating quotient
+    let remainder = dividend%divisor;       //calculating remainder
+    println!("Quotient = {}",quotient);
+    println!("Remainder = {}",remainder);
 }
