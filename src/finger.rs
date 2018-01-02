4dddb3f61701adb655bba5ad21ef88529933cf3e
Farzeen <farseenabdulsalam@gmail.com>
2018-01-02 18:03:30 +0530

    Added some comments. Fixed indentation.

--- src/finger.rs
+++ src/finger.rs
@@ -1,28 +1,32 @@
 use std::io;
 
- fn read()->i32{
-      let mut num=String::new();
-      
-        io::stdin()
-                .read_line(&mut num)
-                .expect("error");
-             num.trim().parse::<i32>()
-                       .unwrap()
- }
- fn main(){
+/* TODO: Figure out what this program does.
+ */
 
-     println!("Enter number");
-     let a=read();
-      
-      let b=a%8;
+fn read() -> i32{
+    let mut num = String::new();
+
+    io::stdin()
+        .read_line(&mut num)
+        .expect("error");
+    num.trim().parse::<i32>()
+        .unwrap()
+}
+
+fn main(){
+
+    println!("Enter number");
+    let a = read();
+
+    let b = a%8;
     if b == 1{
         println!("{}",b);
     }
-    if b == 5{
+    if b == 5 {
         println!("{}",b);
-        }
+    }
     if b == 0 || b == 2{
-       println!("2");
+        println!("2");
     }
     if b == 3 || b == 7{
         println!("3");
@@ -31,5 +35,4 @@ use std::io;
         println!("4");
     }
 
-
- }
+}
