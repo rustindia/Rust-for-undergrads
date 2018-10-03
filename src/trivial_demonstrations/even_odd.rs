215f1b614c512da9db4f82f4af1c32b933288c3c
Sankalp Piratla <42578043+Sankalp00@users.noreply.github.com>
2018-10-04 00:10:15 +0530

    Update even_odd.rs

--- src/trivial_demonstrations/even_odd.rs
+++ src/trivial_demonstrations/even_odd.rs
@@ -3,9 +3,9 @@
  */
 
 fn main() {
-    let x=5;
-    let z=x%2;
-    if z==1 {
+    let x = 5;
+    let z = x % 2;
+    if z == 1 {
         println!("The number {} is odd",x);
     } else {
         println!("The number {} is even",x);
