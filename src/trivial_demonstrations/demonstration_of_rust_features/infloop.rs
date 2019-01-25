c7f2dc9b177f66ab0a656948683b2149e5aad45b
hemanth-hemanth <47024424+hemanth-hemanth@users.noreply.github.com>
2019-01-26 04:53:50 +0530

    infinite loop description

---
+++ src/trivial_demonstrations/demonstration_of_rust_features/infloop.rs
@@ -0,0 +1,11 @@
+fn main(){
+    let mut x=0;
+    
+        loop{
+             x += 5;
+           if  x==100{
+               break;
+           }
+           println!("the value of x ={}",x);
+        }
+}
