39a70aa02086cbc1c31b2cfb0219b73ee06cd4de
nsnk <44540856+nsnk@users.noreply.github.com>
2019-01-26 09:51:21 +0530

    Update sum_of_squares.rs

--- src/trivial_demonstrations/sum_of_squares.rs
+++ src/trivial_demonstrations/sum_of_squares.rs
@@ -1,6 +1,5 @@
 use std::io;
 fn main() {
-    let n:i32;
     let mut num=String::new();
     io::stdin().read_line(&mut num).expect("Error reading number");
     let n = num.trim().parse::<i32>().unwrap();
