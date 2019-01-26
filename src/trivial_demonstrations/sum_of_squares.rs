2c87230a12afccd807896f16137684c68c57c3d4
nsnk <44540856+nsnk@users.noreply.github.com>
2019-01-26 09:30:46 +0530

    Update sum_of_squares.rs

--- src/trivial_demonstrations/sum_of_squares.rs
+++ src/trivial_demonstrations/sum_of_squares.rs
@@ -1,9 +1,9 @@
 use std::io;
 fn main() {
-    let _n:i32;
+    let n:i32;
     let mut num=String::new();
     io::stdin().read_line(&mut num).expect("Error reading number");
-    let _n = num.trim().parse::<i32>().unwrap();
-    let _f:i32=(_n*(_n+1)*(2*_n+1))/6;
-    println!("sum of squares of n natural numbers ={}",_f);
+    let n = num.trim().parse::<i32>().unwrap();
+    let f:i32=(n*(n+1)*(2*n+1))/6;
+    println!("sum of squares of n natural numbers ={}",f);
 }
